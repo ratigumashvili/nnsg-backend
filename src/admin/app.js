af36d86881a820f5b6ee644e97a0f6e20287d54b
ratigumashvili <ratigumashvili@gmail.com>
2025-05-07 05:46:32 +0400

    refactored login screen

---
+++ src/admin/app.js
@@ -0,0 +1,12 @@
+export default {  
+    config: {  
+      translations: {  
+        en: {  
+          "Auth.form.welcome.title": "NNSG",
+          "Auth.form.welcome.subtitle": "Log in to your NNSG admin panel",
+          "Auth.form.forgot-password": "Forgot your password?",
+        },  
+      },  
+    },  
+    bootstrap() {},  
+  };        
